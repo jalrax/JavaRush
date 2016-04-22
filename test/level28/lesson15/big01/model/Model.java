71d4ea1b41155d63835066fa9f615cef1c2b969e
Serge <trymental@gmail.com>
2016-04-22 10:06:14 +0300

    Update

---
+++ test/level28/lesson15/big01/model/Model.java
@@ -0,0 +1,36 @@
+package com.javarush.test.level28.lesson15.big01.model;
+
+import com.javarush.test.level28.lesson15.big01.view.View;
+import com.javarush.test.level28.lesson15.big01.vo.Vacancy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Лена on 10.04.2016.
+ */
+public class Model
+{
+    View view;
+    Provider[] providers;
+
+    public Model(View view, Provider[] providers)
+    {
+        if (view == null || providers == null || providers.length == 0)
+        {
+            throw new IllegalArgumentException("Illegal arguments");
+        }
+        this.view = view;
+        this.providers = providers;
+    }
+
+    public void selectCity(String city)
+    {
+        List<Vacancy> vacancies = new ArrayList<>();
+        for (Provider provider : providers)
+        {
+            vacancies.addAll(provider.getJavaVacancies(city));
+        }
+        view.update(vacancies);
+    }
+}
