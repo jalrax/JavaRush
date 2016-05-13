8648defb76dd65b3e3a9dc285eaa20c707d17498
jalrax <trymental@gmail.com>
2016-05-13 12:43:26 +0300

    Update

--- test/level30/lesson15/big01/Client/Client.java
+++ test/level30/lesson15/big01/Client/Client.java
@@ -6,11 +6,18 @@ import com.javarush.test.level30.lesson15.big01.Message;
 import com.javarush.test.level30.lesson15.big01.MessageType;
 
 import java.io.IOException;
+import java.net.Socket;
 
 public class Client
 {
     protected Connection connection;
     private volatile boolean clientConnected = false;
+
+    public static void main(String[] args)
+    {
+        Client client = new Client();
+        client.run();
+    }
 
     protected String getServerAddress()
     {
@@ -56,7 +63,159 @@ public class Client
         }
     }
 
+    public void run()
+    {
+        SocketThread socketThread = getSocketThread();
+        socketThread.setDaemon(true);
+        socketThread.start();
+
+        synchronized (this)
+        {
+            try
+            {
+                this.wait();
+            }
+            catch (InterruptedException e)
+            {
+                ConsoleHelper.writeMessage("Ошибка выполнения программы.");
+                return;
+            }
+        }
+
+        if (clientConnected)
+        {
+            ConsoleHelper.writeMessage("Соединение установлено. Для выхода наберите команду'exit'.");
+        } else
+        {
+            ConsoleHelper.writeMessage("Произошла ошибка во время работы клиента.");
+        }
+
+        while (clientConnected)
+        {
+            String text = ConsoleHelper.readString();
+            if (text.toLowerCase().equals("exit"))
+            {
+                break;
+            }
+            if (shouldSentTextFromConsole())
+            {
+                sendTextMessage(text);
+            }
+        }
+    }
+
     public class SocketThread extends Thread
     {
+        protected void processIncomingMessage(String message)
+        {
+            ConsoleHelper.writeMessage(message);
+        }
+
+        protected void informAboutAddingNewUser(String userName)
+        {
+            ConsoleHelper.writeMessage(String.format("Участник %s присоединился к чату", userName));
+        }
+
+        protected void informAboutDeletingNewUser(String userName)
+        {
+            ConsoleHelper.writeMessage(String.format("Участник %s покинул чат", userName));
+        }
+
+        protected void notifyConnectionStatusChanged(boolean clientConnected)
+        {
+            Client.this.clientConnected = clientConnected;
+            synchronized (Client.this)
+            {
+                Client.this.notify();
+            }
+        }
+
+        protected void clientHandshake() throws IOException, ClassNotFoundException
+        {
+            boolean accepted = false;
+            while (!accepted)
+            {
+                Message message = connection.receive();
+                switch (message.getType())
+                {
+                    case NAME_REQUEST:
+                        String userName = getUserName();
+                        connection.send(new Message(MessageType.USER_NAME, userName));
+                        break;
+                    case NAME_ACCEPTED:
+                        notifyConnectionStatusChanged(true);
+                        return;
+                    default:
+                        throw new IOException("Unexpected MessageType");
+                }
+            }
+        }
+
+        protected void clientMainLoop() throws IOException, ClassNotFoundException
+        {
+            while (!Thread.currentThread().isInterrupted())
+            {
+                Message message = connection.receive();
+                switch (message.getType())
+                {
+                    case TEXT:
+                        processIncomingMessage(message.getData());
+                        break;
+                    case USER_ADDED:
+                        informAboutAddingNewUser(message.getData());
+                        break;
+                    case USER_REMOVED:
+                        informAboutDeletingNewUser(message.getData());
+                        break;
+                    default:
+                        throw new IOException("Unexpected MessageType");
+                }
+            }
+        }
+
+        public void run()
+        {
+            String ip = getServerAddress();
+            int port = getServerPort();
+            try
+            {
+                Socket socket = new Socket(ip, port);
+                Client.this.connection = new Connection(socket);
+                clientHandshake();
+                clientMainLoop();
+            }
+            catch (IOException e)
+            {
+                notifyConnectionStatusChanged(false);
+            }
+            catch (ClassNotFoundException e)
+            {
+                notifyConnectionStatusChanged(false);
+            }
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test/level30/lesson15/big01/client/ClientGuiController.java
@@ -0,0 +1,88 @@
+package com.javarush.test.level30.lesson15.big01.client;
+
+/**
+ * Created by repin.s on 13.05.2016.
+ */
+public class ClientGuiController extends Client
+{
+    private ClientGuiModel model;
+    private ClientGuiView view;
+
+    public ClientGuiController()
+    {
+        model = new ClientGuiModel();
+        view = new ClientGuiView(this);
+    }
+
+    public static void main(String[] args)
+    {
+        ClientGuiController controller = new ClientGuiController();
+        controller.run();
+    }
+
+    @Override
+    public void run()
+    {
+        SocketThread socketThread = getSocketThread();
+        socketThread.run();
+    }
+
+    @Override
+    protected String getServerAddress()
+    {
+        return view.getServerAddress();
+    }
+
+    @Override
+    protected int getServerPort()
+    {
+        return view.getServerPort();
+    }
+
+    @Override
+    protected String getUserName()
+    {
+        return view.getUserName();
+    }
+
+    @Override
+    protected SocketThread getSocketThread()
+    {
+        return new GuiSocketThread();
+    }
+
+    public ClientGuiModel getModel()
+    {
+        return model;
+    }
+
+    public class GuiSocketThread extends SocketThread
+    {
+        @Override
+        protected void processIncomingMessage(String message)
+        {
+            model.setNewMessage(message);
+            view.refreshMessages();
+        }
+
+        @Override
+        protected void informAboutAddingNewUser(String userName)
+        {
+            model.addUser(userName);
+            view.refreshUsers();
+        }
+
+        @Override
+        protected void informAboutDeletingNewUser(String userName)
+        {
+            model.deleteUser(userName);
+            view.refreshUsers();
+        }
+
+        @Override
+        protected void notifyConnectionStatusChanged(boolean clientConnected)
+        {
+            view.notifyConnectionStatusChanged(clientConnected);
+        }
+    }
+}
